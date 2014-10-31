571ced44221ac7addc841c142d777a3d4cba91cf
Jonathan Niles <jonathanwniles@gmail.com>
2014-10-30 23:59:08 -0400

    Woah.  ERB templated emails?!  Fabulous!

--- routes/email.rb
+++ routes/email.rb
@@ -1,9 +1,10 @@
 require 'erb'
 require 'date'
 
-BLUETEMPLATE = 'email/blue.erb'
-ORANGETEMPLATE = 'email/orange.erb'
-REDTEMPLATE = 'email/red.erb'
+EMAILPATH = "email/"
+BLUETEMPLATE = "email/blue.erb"
+ORANGETEMPLATE = "email/orange.erb"
+REDTEMPLATE = "email/red.erb"
 
 class Emails < Sinatra::Base
 
@@ -14,6 +15,14 @@ class Emails < Sinatra::Base
   get '/email/:date' do
     date = Date.parse(params[:date])
     puts "Date is: #{date}"
+    template = File.read(BLUETEMPLATE)
+    health_center = "SRQ"
+    alerts = [{"name"=>"Desogen","CPT"=>"1234","stock"=>5,"rate"=>2.4,"out"=>date.strftime('%a %d %b %Y')}]
+    renderer = ERB.new(template)
+    result = renderer.result(binding)
+    email = File.new(File.join(EMAILPATH, date.strftime('%Y-%m-%d') + '.html'), "w")
+    email.write(result)
+    email.close
   end
 
 end
