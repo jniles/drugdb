dcab45d28e65f4ffcff92919aad7a54ab7380cb1
Jonathan Niles <jonathanwniles@gmail.com>
2014-11-01 17:01:32 -0400

    What?! Blue, Orange, Red email stock templates.

--- routes/email.rb
+++ routes/email.rb
@@ -7,22 +7,46 @@ ORANGETEMPLATE = "email/orange.erb"
 REDTEMPLATE = "email/red.erb"
 
 class Emails < Sinatra::Base
-
   #
   # Email Routes
   #
   
-  get '/email/:date' do
+  # Blue is for low stock (one month notice)
+  get '/email/blue/:date' do
     date = Date.parse(params[:date])
-    puts "Date is: #{date}"
-    template = File.read(BLUETEMPLATE)
-    health_center = "SRQ"
-    alerts = [{"name"=>"Desogen","CPT"=>"1234","stock"=>5,"rate"=>2.4,"out"=>date.strftime('%a %d %b %Y')}]
-    renderer = ERB.new(template)
-    result = renderer.result(binding)
-    email = File.new(File.join(EMAILPATH, date.strftime('%Y-%m-%d') + '.html'), "w")
-    email.write(result)
+
+    templateString = File.read(BLUETEMPLATE)
+    data = { :health_center => "SRQ", "alerts" => [{:name=>"Desogen",:CPT=>"1234",:stock=>5,:rate=>2.4,:out=>date.strftime('%d %b %Y')}]}
+    template = ERB.new(templateString).result(binding)
+
+    email = File.new(File.join(EMAILPATH, date.strftime('%Y-%m-%d') + '-blue.html'), "w")
+    email.write(template)
     email.close
   end
 
+  # Orange is for ultra-low stock (one week notice)
+  get '/email/red/:date' do
+    date = Date.parse(params[:date])
+
+    templateString = File.read(REDTEMPLATE)
+    data = { :health_center => "SRQ", "alerts" => [{:name=>"Desogen",:CPT=>"1234",:stock=>5,:rate=>2.4,:out=>date.strftime('%d %b %Y')}]}
+    template = ERB.new(templateString).result(binding)
+
+    email = File.new(File.join(EMAILPATH, date.strftime('%Y-%m-%d') + '-red.html'), "w")
+    email.write(template)
+    email.close
+  end
+
+  # Red is stock out complete (zero or negative stock left)
+  get '/email/orange/:date' do
+    date = Date.parse(params[:date])
+
+    templateString = File.read(ORANGETEMPLATE)
+    data = { :health_center => "SRQ", "alerts" => [{:name=>"Desogen",:CPT=>"1234",:stock=>5,:rate=>2.4,:out=>date.strftime('%d %b %Y')}]}
+    template = ERB.new(templateString).result(binding)
+
+    email = File.new(File.join(EMAILPATH, date.strftime('%Y-%m-%d') + '-orange.html'), "w")
+    email.write(template)
+    email.close
+  end
 end
