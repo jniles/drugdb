bc5f72df7641c3868962018ea73b9285f51fbfff
Vinushika <stefan.schalk12@ncf.edu>
2014-11-12 17:00:33 -0500

    Added zero display/hide functionality

---
+++ public/js/drugdisplay.js
@@ -0,0 +1,5 @@
+function show_hide_zero_count(){
+	$(".zero-count").toggle();
+}
+
+$("#zero_count_hide").click(show_hide_zero_count);
--- routes/display.rb
+++ routes/display.rb
@@ -10,16 +10,25 @@ class DrugDisplay < Sinatra::Base
 			all_counts = Count.all(:cpt => cpt, :order => [:date.asc]).to_a #get all counts associated to this one, for plotting, in descending order
 			if all_counts.length > 0
 				#now get all sales for this cpt
+				puts cpt.code
+				puts all_counts[-1].date
 				all_sales = Sale.all(:cpt => cpt, :date.gte => all_counts[-1].date, :order => [:date.asc]) #get all sales that are newer than the last count
-				current_count = {:count => all_counts[-1].count,:date => nil} #this is going to be the one we append to the end of all_counts
 				for sale in all_sales do
-					#subtract off the sales so we get an estimate of where we are today
-					#TODO: maybe add parlvl stuff here?
-					current_count['count'] -= sale.count 
-					current_count['date'] = sale.date
+					puts sale.date
+				end
+				current_count = {:count => all_counts[-1].count,:date => all_counts[-1].date } #this is going to be the one we append to the end of all_counts
+				#we set it to the date of the last count until we can prove sales of the stuff
+				if not all_sales.nil?
+					for sale in all_sales do
+						#subtract off the sales so we get an estimate of where we are today
+						#TODO: maybe add parlvl stuff here?
+						#also add corrections...
+							current_count[:count] -= sale.count 
+							current_count[:date] = sale.date
+					end
 				end
 				all_counts.push(current_count) #make sure we add this to our chart
-			cpt_hash = {:code => cpt.code, :name => cpt.drug.name, :all_counts => all_counts, :current_count => current_count, :vendor => cpt.drug.vendor} #we need to keep all_counts for graphing
+			cpt_hash = {:code => cpt.code, :name => cpt.drug.name, :all_counts => all_counts, :current_count => current_count[:count], :vendor => cpt.drug.vendor, :sales => all_sales} #we need to keep all_counts for graphing
 			else
 				cpt_hash = {:code => cpt.code, :name => cpt.drug.name, :all_counts => nil, :current_count => 0, :vendor => cpt.drug.vendor}
 			end
