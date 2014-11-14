eb1432512997601bb64bd41419daf859b6f37179
Jonathan Niles <jonathanwniles@gmail.com>
2014-11-14 15:54:49 -0500

    Added HTML email confirmation of passwd change

--- routes/account.rb
+++ routes/account.rb
@@ -1,4 +1,10 @@
 require 'securerandom'
+require 'pony'
+
+# Invalid UTF-8 sequences in the email file(s) require this soln
+require 'iconv' unless String.method_defined?(:encode)
+
+PASSWORDEMAIL = '/home/jniles/code/drugdb/email/password.reset.erb';
 
 # FIXME
 #   This module is a duplication of Vinushka's work, before he pushed his
@@ -10,6 +16,15 @@ class Accounts < Sinatra::Base
   # Account Routes
   #
 
+  def escape(string)
+    if String.method_defined?(:encode)
+        string.encode!('UTF-8', 'UTF-8', :invalid => :replace)
+    else
+        ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+        string = ic.iconv(file_contents)
+    end
+  end
+
   set :views, Dir.pwd + "/views"
 
   get '/account/reset' do
@@ -17,21 +32,64 @@ class Accounts < Sinatra::Base
   end
 
   post '/account/reset' do
-    # find the appropriate user
-    user =  User.first(:email => params[:email])
-    if user
-      uuid = SecureRandom.uuid
-      user.update({ :reset_uuid => uuid, :reset_time => Date.new })
-      p uuid
-      Pony.mail({
-        :to => params[:email],
-        :subject => "Your password for the Planned Parenthood Drug Database has been reset",
-        :body => "Your password has been reset to x. Please click the link below to access your account and change your password.",
-        :via => :sendmail
-      })
-      erd :"account/confirmation", :locals => { :email => params[:email] }
-    else
+		user = User.first(:email => params[:email]) #try to find the user
+
+    if user.nil?
       erb :"account/reset", :locals => { :error => params[:email] }
+    else
+
+      if not user.uuid_date or user.uuid_date < Date.today
+        new_uuid = SecureRandom.uuid
+        user.update({ :uuid_token => new_uuid, :uuid_date => Date.today })
+      end
+
+      # compose the email
+      url = "http://localhost:#{settings.port}/account/change/#{new_uuid}"
+
+      templateString = File.read(PASSWORDEMAIL)
+      data = { :url => url, :user => user.name, :email => params[:email] }
+      template = ERB.new(templateString).result(binding)
+
+      # send the email
+      Pony.mail({
+        :from => "admin@drugdb",
+        :to => params[:email],
+        :subject => "Password Reset Request",
+        :html_body => escape(template),
+        :via => :smtp,
+        :via_options => {
+          :address => "smtp.ncf.edu",
+          :port => "25",
+          :authentication => :plain,
+          :domain => "10.10.11.15"
+        }
+      })
+
+      erb :"account/reset_confirmation", :locals => { :data => data }
     end
   end
+
+	get '/account/change/:uuid' do
+		user = User.first(:uuid_token => params[:uuid])
+		if not user.nil?
+		# TODO: Find out how to subtract dates so we can say "UUID less than a week old" or whatever
+		# cool, so yours is valid. Display change form...we make it a template so it has the correct links
+			@uuid = params[:uuid]
+			erb :change
+		else
+			"The link you provided is either invalid or has expired. If you intended to reset your password and have input this link correctly, please try to <a href='/account/reset/'>reset</a> it again."
+		end
+	end
+
+	post '/account/change/:uuid' do
+		user = User.first(:uuid_token => params[:uuid]) #check again just in case
+		if not user.nil?
+			user.update({:password => params[:new_password], :uuid_token => nil, :uuid_date => nil}) #wipe old tokens
+			redirect "/"
+		else
+			"The UUID you have provided is no longer valid. Please try to reset your password again <a href='/account/reset/'>using our automated system.</a>"
+		end
+		# we do the password matching check in JS, not on the server.
+		# now take the new password and shove it in to user
+	end
 end
--- routes/auth.rb
+++ routes/auth.rb
@@ -1,7 +1,4 @@
-require 'pony'
 require 'warden'
-
-require 'securerandom'
 require 'date'
 
 class Auth < Sinatra::Base
@@ -34,16 +31,12 @@ class Auth < Sinatra::Base
 
     # authenticating a user
     def authenticate!
-      # find for user
       user = User.first(name: params['username'])
       if user.nil?
-          puts "[AUTH] No user!"
           fail!("Invalid credentials: user does not exist.")
       elsif user.authenticate(params['password'])
-        puts "[AUTH] Login success!"
         success!(user)
       else
-        puts "[AUTH] Login error!"
         fail!("An error occurred.  Please try again.")
       end
     end
@@ -76,62 +69,4 @@ class Auth < Sinatra::Base
     env['warden'].logout
     redirect '/'
   end
-
-  get '/auth/reset' do
-    send_file File.join('public', 'reset.html')
-  end
-
-  post '/auth/reset' do
-    puts "Sending an email to #{params[:email]}..."
-    # gets 12 pseudorandom characters in the ASCII A-Z + symbols + a-z range
-		user =  User.first(:email => params[:email]) #try to find the user
-		if not user.nil?
-			if not user.uuid_date or user.uuid_date < Date.today
-				new_uuid = SecureRandom.uuid
-				user.update({ :uuid_token => new_uuid, :uuid_date => Date.today })
-			end
-#    arf =(0...12).map { (65 + rand(56)).chr }.join
-#    user.update({ :password => arf })
-			Pony.mail({
-				:from => "admin@drugdb",
-				:to => params[:email],
-				:subject => "Please reset your password for the Planned Parenthood Drug Inventory Databse",
-				:body => "You, or someone claiming to be you, has asked to reset your password. Please go to http://localhost:7999/auth/change/#{new_uuid} to change your password.", #TODO: Change localhost:whatever to our actual URL
-				:via => :smtp,
-				:via_options => {
-					:address => "smtp.ncf.edu",
-					:port => "25",
-					:authentication => :plain,
-					:domain => "10.10.11.15"
-				}
-			})
-			"Sent an email to #{params[:email]} with a link to change your password. Please go to that link now."
-		else
-			"No user was found under this email address. Go <a href='/auth/reset/'>back</a> and try again."
-		end
-  end
-
-	get '/auth/change/:uuid' do
-		user = User.first(:uuid_token => params[:uuid])
-		if not user.nil?
-		#TODO: Find out how to subtract dates so we can say "UUID less than a week old" or whatever
-		#cool, so yours is valid. Display change form...we make it a template so it has the correct links
-			@uuid = params[:uuid]
-			erb :change
-		else
-			"The link you provided is either invalid or has expired. If you intended to reset your password and have input this link correctly, please try to <a href='/auth/reset/'>reset</a> it again."
-		end
-	end
-
-	post '/auth/change/:uuid' do
-		user = User.first(:uuid_token => params[:uuid]) #check again just in case
-		if not user.nil?
-			user.update({:password => params[:new_password], :uuid_token => nil, :uuid_date => nil}) #wipe old tokens
-			redirect "/"
-		else
-			"The UUID you have provided is no longer valid. Please try to reset your password again <a href='/auth/reset/'>using our automated system.</a>"
-		end
-		#we do the password matching check in JS, not on the server.
-		#now take the new password and shove it in to user
-	end
 end
