e7eae4ba65c252baaa52256cfb3cb3299d7b57ee
Jonathan Niles <jonathanwniles@gmail.com>
2015-01-23 16:08:47 -0500

    cleaned up app.js immensely

--- public/js/app.js
+++ public/js/app.js
@@ -30,53 +30,65 @@
     });
   };
 
-  
+
   // MODULE: Drugs
   app.drugs = {};
 
   // sets up listeners for health center selection
   app.drugs.listeners = function () {
 
-    // set up listeners for this module
+    // TODO
+    // These listeners should be written directly in the application
+    // Set up listeners for this module
     $(win.document).on('ready', setupDisplay);
-    $('.zero_count_hide').on('click', toggleZeroCounts);
+
+    $('.zeroCountToggle').on('click', toggleZeroCounts);
+
     $('#hc_select').on('change', changeDisplayedCenter);
 
     $('.hc_table').bind('click', function (event) {
       if (event.target.className == 'chart_link') {
-        var split_id = event.target.id.split('_');
-        addChartRow(split_id[0],split_id[2],split_id[3]);
+        var splitId = event.target.id.split('_');
+        addChartRow(splitId[0],splitId[2],splitId[3]);
       }
-    });	
+    });
 
-    // Toggles the zero count records
+    // Shows or hides zero count records
     function toggleZeroCounts() {
       $('.zero-count').toggle();
     }
 
+    // this function will hide every health center,
+    // then display the health center with id "id"
+    function showHealthCenterTable(id) {
+      $('.hidden').hide();
+      $('#' + id).show();
+    }
+
     function setupDisplay() {
-      var last_displayed = localStorage.getItem('selected_center');
+      var cachedHealthCenter = localStorage.getItem('selectedCenter');
 
-      $('#hc_select').val(last_displayed);
+      if (!cachedHealthCenter) {
 
-      if (last_displayed !== null) {
-        $('.hidden').hide();
-        $('#'+last_displayed).show();
+        // load  a default health center
+        changeDisplayedCenter();
       } else {
-        changeDisplayedCenter(); //do the default instead
+
+        // set the <select> to the cached health center
+        $('#hc_select').val(cachedHealthCenter);
+        showHealthCenterTable(cachedHealthCenter);
       }
     }
 
     function changeDisplayedCenter() {
-      var selected_center = $('#hc_select').val();
-      $('.hidden').hide();
-      $('#'+selected_center).show();
-      localStorage.setItem('selected_center',selected_center);
+      var selectedCenter = $('#hc_select').val();
+      showHealthCenterTable(selectedCenter);
+      localStorage.setItem('selectedCenter', selectedCenter);
     }
 
-    function addChartRow(chart_type, hc_id, cpt) {
+    function addChartRow(chartType, hcId, cpt) {
 
-      var chartString = chart_type + '_chart_'+hc_id+'_'+cpt;
+      var chartString = chartType + '_chart_'+hcId+'_'+cpt;
       if ($('#display_'+chartString).is(':visible')) {
         $('#display_'+chartString).remove();
         return;
@@ -85,27 +97,28 @@
       var newRow = '<tr id=\'display_'+chartString+'\'><td colspan=\'5\'></td></tr>';
 
       // get the drug name from this same row...
-      var drugName = $('#'+chartString).parent().parent().find('.drugName').html();
+      var drugName = $('#'+chartString).parent().parent().find('.drug_name').html();
 
       // go up from <a> to <td> to <tr>, then append
       $('#'+chartString).parent().parent().after($(newRow));
 
       // we decide what to use in the function, then call this one with different args on each td
-      drawGraph(cpt, hc_id, chart_type, drugName);
+      drawGraph(cpt, hcId, chartType, drugName);
     }
 
-    function drawGraph(cpt,hc_id,chart_type,drugName) {
+    function drawGraph(cpt, hcId, chartType, drugName) {
+      var url = ['data', chartType, hcId, cpt].join('/');
 
       // Draws graph with whatever charting library we have.
-      $.getJSON('/data/'+chart_type+'/'+hc_id+'/'+cpt, function renderChart(chartData) {
+      $.getJSON(url, function renderChart(chartData) {
         // hack - need the 0 cause this uses regular DOM element objects, not jQuery objects
-        var obj = $('#display_'+chart_type+'_chart_'+hc_id+'_'+cpt+' td');
+        var obj = $('#display_'+chartType+'_chart_'+hcId+'_'+cpt+' td');
 
         // Render the chart
         obj.highcharts({
           title : {
             // drug names are all caps anyway...
-            text: 'Consumption for ' + drugName, 
+            text: 'Consumption for ' + drugName,
             x: -20
           },
           xAxis: {
